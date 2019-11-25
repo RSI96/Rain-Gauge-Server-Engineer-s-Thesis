e363682fbf9f35b0446875b2aceb42ef7d7ec71c
radek.sikora <radek.sikora@bitcave.eu>
2019-11-25 15:18:53 +0100

    server done

---
+++ index.js
@@ -0,0 +1,136 @@
+const {Client} = require('pg');
+const express = require('express');
+
+Date.prototype.withoutTimezone = function () {
+    this.setMilliseconds(this.getMilliseconds() - this.getTimezoneOffset() * 1000 * 60);
+    return this;
+};
+
+Array.prototype.mapDateTimezone = function (key) {
+    return this.map((data) => {
+        data[key] = new Date(data[key]).withoutTimezone();
+        return data
+    });
+};
+
+Array.prototype.groupByKey = function (key) {
+    return this.reduce(function (r, a) {
+        r[a[key]] = r[a[key]] || [];
+        r[a[key]].push(a);
+        return r;
+    }, Object.create(null));
+};
+
+const app = express();
+app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+const PORT = process.env.PORT || 8080;
+
+const client = new Client({
+    user: "postgres",
+    password: "deszczomierz",
+    host: "localhost",
+    port: 5432,
+    database: "deszczomierz_db"
+});
+
+client.setTypeParser(1082, function (stringValue) {
+    return stringValue;
+});
+
+app.listen(PORT, () => console.log(`My server is listening on port ${PORT}`));
+
+app.get('/all', async (req, res) => {
+    const rows = await readAll();
+    res.send(rows)
+});
+
+app.get('/lastHour', async (req, res) => {
+    const rows = await readLastHour();
+    res.send(rows)
+});
+
+app.get('/lastDay', async (req, res) => {
+    const rows = await loadLastDay();
+    res.send(rows)
+});
+
+app.get('/lastWeek', async (req, res) => {
+    const rows = await loadLastWeek();
+    res.send(rows)
+});
+
+app.post('/all', async (req, res) => {
+    let result = {};
+    try {
+        console.log(req.body);
+        const reqJson = req.body;
+        await addMeasurement(reqJson.id, reqJson.measurement, reqJson.date, reqJson.time, reqJson.date_time);
+        result.success = true
+    } catch (e) {
+        result.success = false
+    } finally {
+        res.send(result)
+    }
+});
+
+connect();
+
+async function connect() {
+    try {
+        await client.connect();
+    } catch (e) {
+        console.error("Failed to connect to Postgres DB due to:" + e)
+    }
+}
+
+async function addMeasurement(id, measurement, date, time, date_time) {
+    try {
+        await client.query("insert into rain_gauge_data (id, measurement, date, time, date_time) values ($1, $2, $3, $4, $5)", [id, measurement, date, time, date_time]);
+        return true
+    } catch (e) {
+        return false
+    }
+}
+
+async function readAll() {
+    try {
+        const results = await client.query("select * from rain_gauge_data");
+        return results.rows
+    } catch (e) {
+        return []
+    }
+}
+
+async function readLastHour() {
+    try {
+        const results = await client.query("select coalesce(sum(measurement), 0) as suma from rain_gauge_data where time >= localtime - $1 * interval '1 hour'", [1]);
+        return results.rows
+    } catch (e) {
+        return []
+    }
+}
+
+async function loadLastDay() {
+    try {
+        const results = await client.query("select series as date_time, cast(series::timestamp as time) as hour, coalesce(sum(measurement), 0) as suma from generate_series( date_trunc('hour', now()::timestamp) - '23 hours'::interval, date_trunc('hour', now()::timestamp), '1 hour'::interval ) as series left join rain_gauge_data on date_trunc('hour', rain_gauge_data.date_time) = series and now()::timestamp >= date_time - INTERVAL '1 DAY' group by 1 order by series");
+        return results.rows.mapDateTimezone("date_time");
+    } catch (e) {
+        return []
+    }
+}
+//select hour, coalesce(sum(measurement), 0) as suma from generate_series( date_trunc('hour', now()::timestamp) - '23 hours'::interval, date_trunc('hour', now()::timestamp), '1 hour'::interval ) as hour left join rain_gauge_data on date_trunc('hour', rain_gauge_data.date_time) = hour and now()::timestamp >= date_time - INTERVAL '1 DAY' group by 1 order by hour
+// select series as date_time, cast(series::timestamp as time) as hour, coalesce(sum(measurement), 0) as suma from generate_series( date_trunc('hour', now()::timestamp) - '23 hours'::interval, date_trunc('hour', now()::timestamp), '1 hour'::interval ) as series left join rain_gauge_data on date_trunc('hour', rain_gauge_data.date_time) = series and now()::timestamp >= date_time - INTERVAL '1 DAY' group by 1 order by series
+async function loadLastWeek() {
+    try {
+        const results = await client.query("select T.date, T.hour, T.date + T.hour as date_time, coalesce(sum(measurement), 0) as suma from (select DATE(date) as date, cast (hour::timestamp as time) as hour, DATE(date) + cast (hour::timestamp as time) as date_time from generate_series( current_date - '7 days'::interval, current_date, '1 day'::interval ) as date cross join generate_series(date_trunc('day', now()) - '23 hours'::interval, date_trunc('day', now()),    '1 hour'::interval   ) as hour) AS T left join rain_gauge_data on date_trunc('hour', rain_gauge_data.time) = T.hour and T.date = rain_gauge_data.date group by T.date, T.hour order by T.date, T.hour");
+        return results.rows.mapDateTimezone("date_time").groupByKey("date");
+    } catch (e) {
+        return []
+    }
+}
+
